8d591c78330bfcc03ffba6bf3dcc68313ef7b5bb
amolwaghmare197 <62028879+amolwaghmare197@users.noreply.github.com>
2021-11-01 12:03:54 +0530

    Add files via upload

---
+++ additionprog.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package c1;
+import java.util.*;
+/**
+ * @author JOHN
+ *
+ */
+public class additionprog {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("please enter the two no");
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println("fno " + a);
+        System.out.println("secno " + b);
+        System.out.println("add =" + c);
+	}
+
+}
